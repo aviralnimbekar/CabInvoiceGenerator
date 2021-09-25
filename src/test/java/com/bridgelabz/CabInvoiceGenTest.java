f697705534a51ab780e8602049c51004990c33ad
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-25 12:16:41 +0530

    REFACTORED : Created a setup to initialise instance before each test

--- src/test/java/com/bridgelabz/CabInvoiceGenTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGenTest.java
@@ -1,25 +1,30 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGenTest {
+    CabInvoiceGen cabInvoiceGen;
+
+    @BeforeEach
+    void toCreateInstanceOfCabInvoiceGenBeforeRunningTest() {
+        cabInvoiceGen = new CabInvoiceGen();
+    }
 
     @Test
     void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGen cabInvoiceGen = new CabInvoiceGen();
         double distance = 2.0;
         int time = 5;
-        double fare = cabInvoiceGen.calculateFare(distance,time);
+        double fare = cabInvoiceGen.calculateFare(distance, time);
         Assertions.assertEquals(25, fare);
     }
 
     @Test
     void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-        CabInvoiceGen cabInvoiceGen = new CabInvoiceGen();
         double distance = 0.1;
         int time = 1;
-        double minFare = cabInvoiceGen.calculateFare(distance,time);
+        double minFare = cabInvoiceGen.calculateFare(distance, time);
         Assertions.assertEquals(5, minFare);
     }
 }
